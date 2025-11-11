5c587fc2a4ea59745ddc38d6db6f25d2546fa13e
EmkoDelirdi <73055059+emkodelirdi@users.noreply.github.com>
2025-11-12 00:29:29 +0300

    fixed the same issue for Path

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using System.Collections;
 
 public class Path : MonoBehaviour
 {
     public Transform[] points;
 
-    void Start()
+    IEnumerator Start()
     {
+        while (EnemyManager.inst == null)
+            yield return null;
         EnemyManager.inst.SetPath(points);
     }
 }
