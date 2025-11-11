5b68bab346d15d0c981e193eeb791f0cef4e6e22
EmkoDelirdi <73055059+emkodelirdi@users.noreply.github.com>
2025-11-11 22:15:42 +0300

    Started working on enemy + path

---
+++ Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+    public float speed = 10;
+    public float health = 10;
+    public float damage = 10;
+
+    private Transform[] path;
+    private int currentIndex = 0;
+
+
+    public void SetPath(Path pathObj)
+    {
+        path = pathObj.points;
+        currentIndex = 0;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (path == null ||)
+        float dt = Time.deltaTime;
+        
+
+    }
+    
+    public bool TakeDamage(float dmgTaken)
+    {
+        health -= dmgTaken;
+        if (health <= 0)
+        {
+            health = 0;
+            return true;
+        }
+        return false;
+    }
+}
